7185aee5dff820b285999f6f527c60d68446769e
nohelia <ntacca@unsa.edu.pe>
2022-10-13 16:24:15 -0500

    probando examen entrada

--- poo/poo.cc
+++ poo/poo.cc
@@ -2,12 +2,42 @@
 class A{
   public:
     void foo(){
-      std::cout << "B" << std::endl;
+      std::cout << "A" << std::endl;
     } 
 };
+class B: public A{
+  public:
+    void foo(){
+      std::cout << "B" << std::endl;
+    }
+};
+class C: public A{
+  public:
+    void goo(){
+      std::cout << "C" << std::endl; 
+    }
+};
+class D: public B{
+  public:
+    void goo(){
+      std::cout << "D" << std::endl;
+    }
+};
+class E: public C{
+  public:
+    void foo(){
+      std::cout << "E" << std::endl;
+    }
+};
 int main(){
-  A a;
-  a.foo();
+  E e;
+  e.goo();
+
+  A *a = new C();
+  a->foo();
+
+  D d;
+  d.foo();
 }
 
 
